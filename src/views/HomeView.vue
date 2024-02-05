03b9a531dfc431f02ece1b71de7ed98accb135d4
Felipe Martins Duarte <felipemartinsduarte@outlook.com.br>
2024-02-04 22:35:50 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="home">
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 align-self-center">
+            <a href="#" class="logo">
+              <img src="../assets/images/logo.png" alt="logo">
+            </a>
+          </div>
+          <div class="col-md-6 align-self-center">
+            <div class="select-language">
+              <p>Selecionar Idioma:</p>
+              <select>
+                <option value="port">Português</option>
+                <option value="eng">English</option>
+                <option value="heb">Hebrew</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <section class="banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 order-2 order-md-1">
+            <div class="content-wrapper">
+              <h1>transfira <span>rapidamente</span> <br>
+                links e arquivos entre <br>
+                um aparelho e outro</h1>
+              <div class="device-wrapper">
+                <div class="device-card">
+                  <div class="icon-wrapper">
+                    <img src="../assets/images/icons/airplane-icon.svg" alt="icon">
+                  </div>
+                  <p>Passe tudo de um dispositivo para o outro na Velocidade da Luz</p>
+                </div>
+                <div class="device-card">
+                  <div class="icon-wrapper">
+                    <img src="../assets/images/icons/circle-cross-icon.svg" alt="icon">
+                  </div>
+                  <p>Não Precisa de Login ou Registro</p>
+                </div>
+              </div>
+              <div class="device-wrapper">
+                <div class="device-card">
+                  <div class="icon-wrapper">
+                    <img src="../assets/images/icons/guard-icon.svg" alt="icon">
+                  </div>
+                  <p>Sistema Seguro, Nenhum Dado é Mantído quando a conexão entre os dispositivos é cortada</p>
+                </div>
+                <div class="device-card">
+                  <div class="icon-wrapper">
+                    <img src="../assets/images/icons/police-icon.svg" alt="icon">
+                  </div>
+                  <p>Não nos Responsabilizamos pelo tipo de transferência no Website</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 order-1 order-md-2">
+            <div class="barcode-main-wrapper">
+              <div class="barcode-wrapper">
+                <nav>
+                  <div
+                      id="nav-tab"
+                      class="nav nav-tabs border-0 choose-plan-wrapper"
+                      role="tablist"
+                  >
+                    <label
+                        id="nav-home-tab"
+                        for="price-plan"
+                        class="nav-link active p bg-transparent border-0"
+                        data-bs-toggle="tab"
+                        data-bs-target="#individual-tab"
+                        type="button"
+                        role="tab"
+                        aria-controls="individual-tab"
+                        aria-selected="true"
+                    >enviar</label>
+                    <span class="check-btn price-toggle active" />
+
+                    <label
+                        id="nav-profile-tab"
+                        for="price-plan"
+                        class="nav-link p bg-transparent border-0"
+                        data-bs-toggle="tab"
+                        data-bs-target="#enterprise-tab"
+                        type="button"
+                        role="tab"
+                        aria-controls="enterprise-tab"
+                        aria-selected="false"
+                    >receber</label>
+                  </div>
+                </nav>
+                <div id="nav-tabContent" class="tab-content">
+                  <div
+                      id="individual-tab"
+                      class="tab-pane fade show active"
+                      role="tabpanel"
+                      aria-labelledby="nav-home-tab"
+                  >
+                    <div class="barcode-img">
+                      <img src="../assets/images/barcode-img.png" alt="img">
+                    </div>
+                    <a href="#" class="generated-link">Https://TRANSFERINDA.COM/WEBSOCKE...</a>
+                  </div>
+                  <div
+                      id="enterprise-tab"
+                      class="tab-pane fade"
+                      role="tabpanel"
+                      aria-labelledby="nav-profile-tab"
+                  >
+                    <div class="barcode-img">
+                      <img src="../assets/images/barcode-img.png" alt="img">
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <p>Atenção! Não mantemos nenhum dado em nosso sistema, arquivo ou link, utilize seu link privado como preferir.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="about-us">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 order-2 order-md-1">
+            <ul class="steps">
+              <li>
+                <h3 class="h3">como funciona?</h3>
+                <p>Transferinda foi criada com o simples propósito
+                  de transferir LINK(S) e ARQUIVO(S) diretamente entre
+                  quaisquer dispositivos de forma rapída e segura, faça
+                  transferência de link(s) ou arquivo(s) entre quaisquer
+                  dispositivos entre os paises listados que possa acessar
+                  a Transferinda e fazer Download. Transfira do seu Celular
+                  para o Computador ou Inverta na Opção inverter para Transferir
+                  do Seu Computador para o Celular sendo possivel também fazer envio
+                  do link do QRCODE para quaisquer pessoa te fazerem transferência de
+                  algum LINK(s) ou ARQUIVO(S) diretamente sem trocar informações de maneira
+                  super veloz e segura</p>
+              </li>
+              <li>
+                <h3 class="h3">como te protegemos!</h3>
+                <p>Quando o QRCODE é escaneado os dois dispostivos se conectam para que
+                  possa enviar o que desejar, está conexão será mantida por no máximo
+                  24h de transferência de arquivos pesados ou por 20m sem nenhum envio
+                  de arquivo. após isso todos arquivos e dados da conexão serão excluidos. </p>
+              </li>
+              <li>
+                <h3 class="h3">como mantemos o serviço?</h3>
+                <p>Utilizamos Poderosos Servidores com Banco de Dados que apenas podemos ter
+                  através da Monetização de nosso Website </p>
+              </li>
+            </ul>
+            <div class="social-wrapper">
+              <ul class="social">
+                <li>
+                  <a href="#" target="_blank">
+                    <img src="../assets/images/icons/instagram-icon.png" class="instagram" alt="icon">
+                    @transferinda
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_blank">
+                    <img src="../assets/images/icons/twitter-icon.png" alt="icon">
+                    @transferinda
+                  </a>
+                </li>
+              </ul>
+              <h3>Acompanhe novas atualizações</h3>
+            </div>
+          </div>
+          <div class="col-md-6 order-1 order-md-2">
+            <div class="about-img">
+              <img src="../assets/images/about-us-img.png" alt="img">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 order-2 order-md-1 align-self-center">
+            <ul class="links">
+              <li>Contato: <a href="mailto:suporte@transferinda.com">suporte@transferinda.com</a></li>
+              <li>Todos direitos reservados a Transferinda ©</li>
+            </ul>
+          </div>
+          <div class="col-md-6 order-1 order-md-2 align-self-center">
+            <ul class="social">
+              <li>
+                <a href="#" target="_blank">
+                  <img src="../assets/images/icons/footer-instagram-icon.png" class="instagram" alt="icon">
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank">
+                  <img src="../assets/images/icons/footer-twitter-icon.png" alt="icon">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+  },
+});
+</script>
